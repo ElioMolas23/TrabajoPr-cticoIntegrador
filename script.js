ffc98fb8585db44288811bd375cc35393ad74860
ElioMolas23 <eliomolas14@hotmail.com>
2023-04-08 19:31:08 -0300

    terminando responsive y formulario

--- script.js
+++ script.js
@@ -14,3 +14,32 @@
 })(document);
 
 
+//funcion para el formulario
+((d) => {
+  const form = d.querySelector(".contacto-form");
+  const respuesta = d.querySelector(".respuesta-contacto");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("https://formsubmit.co/ajax/eliomolas14@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#gracias";
+        form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+        respuesta.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 5000);
+      });
+  });
+})(document);
